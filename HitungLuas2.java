780533b52d1aace6c6d47fc45cdeb4cd8216efe2
Yulian Adi Wibowo <tim.eagle.1@gmail.com>
2017-01-26 23:08:39 +0700

    Perubahan kedua

---
+++ HitungLuas2.java
@@ -0,0 +1,15 @@
+public class HitungLuas2 {
+    public static void main (String[] args){
+int panjang = 5;
+int lebar = 4;
+float keliling;
+float luas;
+System.out.println("panjangnya="+panjang);
+System.out.println("lebarnya="+lebar);
+keliling =  (float) 2*(panjang+lebar);
+            System.out.println("Jadi keliling persegi panjang adalah="+ panjang+lebar*2);
+luas =  (float) (panjang*lebar);
+            System.out.println("luasnya adalah="+ panjang*lebar);
+
+}
+} 
